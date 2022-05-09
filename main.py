ef4a0744f2e7879a47e4af059b5df52dc4baaa47
Kevin Ochoa Xopa <kevinochoaxopa@gmail.com>
2022-05-09 05:36:02 -0500

    request_response_body

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
+# Python
+from typing import Optional
 
-from fastapi import FastAPI;
+#  Pydantic
+from pydantic import BaseModel
+
+# FastAPI
+from fastapi import FastAPI, Body;
 
 app = FastAPI()
+
+
+# Models
+class Person(BaseModel):
+    first_name: str
+    last_name: str
+    age: int
+    hair_color: Optional[str] = None
+    is_married: Optional[bool] = None
+
+
 
 @app.get("/")
 def home():
     return {'Hello': 'world'}
+
+
+
+# Request and Response Body
+@app.post("/person/new")
+def create_person(person: Person = Body(...)):
+    return person
