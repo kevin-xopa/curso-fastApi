d0d5d296dead959ac6a0bd1da5aafad9919db117
Kevin Ochoa Xopa <kevinochoaxopa@gmail.com>
2022-05-12 23:50:05 -0500

    summary_and_description

--- main.py
+++ main.py
@@ -118,9 +118,21 @@ def home():
     path="/person/new",
     response_model=PersonOut,
     status_code=status.HTTP_201_CREATED,
-    tags=["Persons"]
+    tags=["Persons"],
+    summary="Create Person in the app"
 )
 def create_person(person: Person = Body(...)):
+    """
+        Create Person
+
+        This path operation creates a person in the app and save the information in the database
+
+        Parameters:
+        - Request body parameter:
+            - **person: Person** -> A person model with first name, last name, age, hair color and marital status
+
+        Returns a person model with firs name, last name, age, hair color, and marital status
+    """
     return person
 
 # Validations: Query parameters
@@ -221,7 +233,7 @@ def login(
 @app.post(
     path="/contact",
     status_code=status.HTTP_200_OK,
-    tags = ["Cookies", "Headers"]
+    tags=["Cookies", "Headers"]
 )
 def contact(
     firs_name: str = Form(
